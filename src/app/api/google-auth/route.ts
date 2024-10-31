ba59ffb8d369864cd1e04d3210963e66ce870118
Zane Kharitonov <zane@turnkey.io>
2024-10-31 14:33:29 -0400

    absoulte routing from server components

--- src/app/api/google-auth/route.ts
+++ src/app/api/google-auth/route.ts
@@ -2,6 +2,8 @@
 
 import { NextResponse } from 'next/server';
 
+const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
+
 export async function POST(req: Request) {
   try {
     const body = await req.text();
@@ -9,28 +11,30 @@ export async function POST(req: Request) {
 
     // To access parameters, use:
     const credential = params.get('credential');
+    console.log(credential)
 
     const oidcToken = credential?.split(".")[1]
+    console.log(oidcToken)
 
     if (!oidcToken) {
       // redirect to google oauth page
       const queryParams = new URLSearchParams({
         error: "Failed google oauth1",
       }).toString();
-      NextResponse.redirect(`/auth?${queryParams}`);
+      NextResponse.redirect(`${NEXT_PUBLIC_SITE_URL}/auth?${queryParams}`);
     }
 
     // redirect to google oauth page
     const queryParams = new URLSearchParams({
       oidcToken: oidcToken!,
     }).toString();
-    NextResponse.redirect(`/google-auth?${queryParams}`)
+    NextResponse.redirect(`${NEXT_PUBLIC_SITE_URL}/google-auth?${queryParams}`)
   } catch (e) {
     // redirect to google oauth page
     const queryParams = new URLSearchParams({
       error: "Failed google oauth2",
     }).toString();
 
-    NextResponse.redirect(`/auth?${queryParams}`);
+    NextResponse.redirect(`${NEXT_PUBLIC_SITE_URL}/auth?${queryParams}`);
   }
 }
