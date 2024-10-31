100495e6bf639e77bbb4be99fc8fff63ed654b1e
Zane Kharitonov <zane@turnkey.io>
2024-10-31 02:59:34 -0400

    dont think is gonna work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 
 import Auth from "./auth/page";
+import { NextResponse } from "next/server";
 
 export default function Home() {
   return (
     <Auth/>
   );
+}
+
+export async function POST(request: Request) {
+  console.log("Test")
+
+  return new NextResponse()
 }
