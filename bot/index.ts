9e97a12f1eb714bd77f536bfd76b502f6c60b1a4
Zane Kharitonov <zane@turnkey.io>
2024-10-30 21:49:51 -0400

    serverless bot

--- bot/index.ts
+++ bot/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import { Bot, InlineKeyboard } from "grammy";
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { Update } from "grammy/types";
 
 dotenv.config()
 
@@ -8,19 +10,28 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const bot = new Bot(BOT_TOKEN!);
 
-const labelDataPairs = [
-  ["Degen Trading App (Internal to Telegram)", BOT_DOMAIN!]
-];
-
 const keyboard = new InlineKeyboard()
   .webApp("TurntCoin 🔑", BOT_DOMAIN!)
 
 const replyText = "Welcome to TurntCoin - A demo mini app built on Turnkey 🔑🚀";
 
-bot.on("message", async (ctx) => {
+bot.on("message", async (ctx: any) => {
   await ctx.reply(replyText, {
     reply_markup: keyboard,
   })
 })
 
 bot.start();
+
+export const startBot = async (req: VercelRequest, res: VercelResponse) => {
+  if (!BOT_DOMAIN) {
+    throw new Error('BOT_DOMAIN is not set.');
+  }
+
+  if (req.method === 'POST') {
+    await bot.handleUpdate(req.body as unknown as Update);
+  } else {
+    res.status(200).json('Listening to bot events...');
+  }
+  res.status(200)
+};
