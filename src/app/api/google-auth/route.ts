66a81e5948cb6c68eb145388df23c6c34f26d90a
Zane Kharitonov <zane@turnkey.io>
2024-10-31 11:30:29 -0400

    Just what we need

--- src/app/api/google-auth/route.ts
+++ src/app/api/google-auth/route.ts
@@ -4,13 +4,9 @@ import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   console.log(req)
-  const { credential, clientId, client_id, select_by, g_csrf_token } = await req.json()
+  const { credential } = await req.json()
 
   console.log(credential);
-  console.log(clientId);
-  console.log(client_id);
-  console.log(select_by);
-  console.log(g_csrf_token);
 
   console.log(await req.json());
 
