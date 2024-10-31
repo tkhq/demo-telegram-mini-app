a31e49d08897594323ac533ff4805ef5f48ef41e
Zane Kharitonov <zane@turnkey.io>
2024-10-31 11:43:37 -0400

    Try the search params

--- src/app/api/google-auth/route.ts
+++ src/app/api/google-auth/route.ts
@@ -4,11 +4,15 @@ import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   console.log(req)
-  const { credential } = await req.json()
 
-  console.log(credential);
+  const url = new URL(req.url)
+  const params = url.searchParams;
+  console.log(url)
 
-  console.log(await req.json());
+  console.log("Entries: ")
+  params.forEach((entry) => {
+    console.log(entry)
+  })
 
   return NextResponse.json({ status: 200})
 }
