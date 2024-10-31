5c95f30c9f4ed829f2f850dfb4d10ccc5c80d044
Zane Kharitonov <zane@turnkey.io>
2024-10-31 11:26:35 -0400

    Some debug info

--- src/app/api/google-auth/route.ts
+++ src/app/api/google-auth/route.ts
@@ -4,4 +4,15 @@ import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   console.log(req)
+  const { credential, clientId, client_id, select_by, g_csrf_token } = await req.json()
+
+  console.log(credential);
+  console.log(clientId);
+  console.log(client_id);
+  console.log(select_by);
+  console.log(g_csrf_token);
+
+  console.log(await req.json());
+
+  return NextResponse.json({ status: 200})
 }
