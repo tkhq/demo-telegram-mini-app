83fafe59cece901c3da4f34ded30edd704825837
Zane Kharitonov <zane@turnkey.io>
2024-10-31 03:03:29 -0400

    fix "/" route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 'use client'
 
 import Auth from "./auth/page";
-import { NextResponse } from "next/server";
 
 export default function Home() {
   return (
     <Auth/>
   );
-}
-
-export async function POST(request: Request) {
-  console.log("Test")
-
-  return new NextResponse()
 }
