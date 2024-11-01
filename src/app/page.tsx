f866caa177d2f38f280ab984398b32afa2b6f68d
Zane Kharitonov <zane@turnkey.io>
2024-10-31 21:27:18 -0400

    Fix logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 'use client'
 
+import { useEffect } from "react";
 import Auth from "./auth/page";
 import DisplayTelegramMessage from "./display-telegram/page";
+import { useState } from "react";
 
 export default function Home() {
-  return typeof window !== "undefined" && window.Telegram?.WebApp?.CloudStorage ? (
-    <Auth/>
-  ) : (
-    <DisplayTelegramMessage />
+  const [telegramContext, setTelegramContext] = useState(true);
+  
+  useEffect(() => {
+    try {
+      window.Telegram?.WebApp?.CloudStorage.getItem("test") 
+    } catch (e) {
+      setTelegramContext(false)
+    }
+  }, [])
+
+  return (
+    telegramContext ? 
+      <Auth/> 
+    : 
+      <DisplayTelegramMessage />
   )
 }
