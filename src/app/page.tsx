fa6e5f7d93e17054de0522f2bfcf35c87bea090d
Zane Kharitonov <zane@turnkey.io>
2024-10-31 21:45:21 -0400

    Last try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 'use client'
 
-import { useEffect } from "react";
 import Auth from "./auth/page";
 import DisplayTelegramMessage from "./display-telegram/page";
-import { useState } from "react";
 
 export default function Home() {
-  const [telegramContext, setTelegramContext] = useState(true);
-  
-  useEffect(() => {
-    try{
-      window.Telegram?.WebApp.CloudStorage.getItem("Test")
-    } catch (e) {
-      setTelegramContext(false)
-    }
-  }, [window.Telegram?.WebApp])
-
   return (
-    telegramContext ? 
+    window.Telegram?.WebApp?.version == '6.0' ? 
       <Auth/> 
     : 
       <DisplayTelegramMessage />
