b4f99647d66b873dd90b710073176add43d88c6f
Zane Kharitonov <zane@turnkey.io>
2024-10-31 21:21:14 -0400

    Force users to be on telegram

---
+++ src/app/display-telegram/page.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/card"
+import Link from "next/link"
+
+export default function DisplayTelegramMessage() {
+  return (
+    <div className="min-h-screen bg-foreground flex items-center justify-center p-4">
+      <Card className="w-full max-w-md bg-background">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold text-center">
+            This app is meant for telegram
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-center text-sm text-muted-foreground">
+            This example telegram mini app powered by Turnkey can be found at this bot link <Link href="https://t.me/TurntCoinBot" className="font-semibold underliner">@TurntCoinBot</Link>, try messaging it!
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import Auth from "./auth/page";
+import DisplayTelegramMessage from "./display-telegram/page";
 
 export default function Home() {
-  return (
+  return typeof window !== "undefined" && window.Telegram?.WebApp?.CloudStorage ? (
     <Auth/>
-  );
+  ) : (
+    <DisplayTelegramMessage />
+  )
 }
