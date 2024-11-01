4b13e08d426f38e7dc51e6f575b6e0bc481cafbb
Zane Kharitonov <zane@turnkey.io>
2024-10-31 21:51:25 -0400

    Ok fr last one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import DisplayTelegramMessage from "./display-telegram/page";
 export default function Home() {
   return (
     typeof window !== "undefined" && window?.Telegram?.WebApp?.version == '6.0' ? 
-      <Auth/> 
-    : 
       <DisplayTelegramMessage />
+    : 
+      <Auth/> 
   )
 }
