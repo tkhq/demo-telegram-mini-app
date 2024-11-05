53e963731f347fa65044b0dfb14fe27ed7772d49
Zane Kharitonov <zane@turnkey.io>
2024-11-05 15:35:50 -0500

    Remove async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://telegram.org/js/telegram-web-app.js" async></script>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
       </head>
       <body
         className={`antialiased`}
